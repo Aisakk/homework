d3685d7d6c4e30df757084d49a9049932700b536
Aisakk <santaellaisaac2@gmail.com>
2020-06-12 22:00:51 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import SubFooter from './components/subfooter/SubFooter';
+import Form from './components/form/form';
+import team from './img/team-viewer.png';
+import demo from './img/demodesk.png';
+import urban from './img/urban-sport.png';
+import loyalty from './img/Loyalty.png';
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+          <Header />
+          <div className="body">
+              <Form />
+          </div>
+          <SubFooter/>
+          <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
